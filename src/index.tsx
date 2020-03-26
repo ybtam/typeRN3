e1babc55ed407e820e98406c98d7dc1ce646ce11
yitam <tam.yi3d@gmail.com>
2020-03-26 18:04:53 +0100

    init

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AppNavigator from "./navigation/navigation";
+import {ThemeProvider} from "react-native-elements";
+import {HttpLink, InMemoryCache, ApolloClient, ApolloProvider} from "@apollo/client";
+
+const link = new HttpLink({
+    uri:"https://zjo9h3bqle.execute-api.eu-west-2.amazonaws.com/dev/graphql",
+    credentials: 'include'
+});
+
+const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+});
+
+export default () => (
+    // <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <ApolloProvider client={client}>
+            <ThemeProvider>
+                <AppNavigator/>
+            </ThemeProvider>
+        </ApolloProvider>
+    // </View>
+);
