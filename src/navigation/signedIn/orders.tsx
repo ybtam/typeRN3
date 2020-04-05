b25a000913542b7ed035ffda5a3175bbe0adbb91
yitam <tam.yi3d@gmail.com>
2020-04-05 13:51:57 +0200

    added a new page

--- src/navigation/signedIn/orders.tsx
+++ src/navigation/signedIn/orders.tsx
@@ -1,6 +1,8 @@
 import {createStackNavigator} from "@react-navigation/stack";
 import React from "react";
 import Orders from "../../screens/signedIn/orders/orders";
+import CreateOrder from "../../screens/signedIn/orders/createOrder";
+import Order from "../../screens/signedIn/orders/order";
 
 const orders = createStackNavigator();
 
@@ -8,7 +10,8 @@ export default function ordersStack() {
     return(
         <orders.Navigator>
             <orders.Screen name={"Orders"} component={Orders}/>
-            {/*<orders.Screen name={"Order"} component={Order}/>*/}
+            <orders.Screen name={"Order"} component={Order}/>
+            <orders.Screen name={"CreateOrder"} component={CreateOrder} options={{title:"Create Order"}}/>
         </orders.Navigator>
     );
 }
