3198f0f1c6821f4eb44202d7e08c410e730d88a0
yitam <tam.yi3d@gmail.com>
2020-04-05 13:48:14 +0200

    adding new interfaces

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -11,9 +11,42 @@ export interface company {
 
 export interface order {
     id: number,
+    delivery: boolean,
+    deliveryCost: number,
+    subtotal: number,
     total: number,
     company: company
+    orderProducts: [orderProduct]
+}
 
+export interface orderProduct {
+    id: number,
+    quantity: number,
+    total: number,
+    product: product
+}
+
+export interface product {
+    id: number,
+    code: string,
+    description: string,
+    category: category,
+    allPrices: [price],
+    twoPrice: [price],
+    nowPrice: price
+}
+
+export interface price {
+    id: number,
+    price: number,
+    createAt: Date
+}
+
+export interface category {
+    id: number,
+    name: string,
+    description: string,
+    products: [product]
 }
 
 export interface project {
@@ -32,4 +65,10 @@ export interface checkOut {
     id: number,
     createAt: Date
     minutes_worked: number
+}
+
+export interface task {
+    id: number,
+    title: string,
+    completed: boolean
 }
