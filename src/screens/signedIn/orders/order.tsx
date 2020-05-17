32d8ae6eb9ef53a834279ad74b91883bfb557c06
yitam <tam.yi3d@gmail.com>
2020-05-17 18:56:34 +0200

    added a add product button

--- src/screens/signedIn/orders/order.tsx
+++ src/screens/signedIn/orders/order.tsx
@@ -3,68 +3,77 @@ import {Button, Card, ListItem, Text} from "react-native-elements";
 import {useQuery} from "@apollo/client";
 import {order_query} from "../../../graphql/queries";
 import {order, orderProduct} from "../../../interfaces";
-import {FlatList} from "react-native";
+import {FlatList, RefreshControl, ScrollView, View} from "react-native";
+import {tabScreenStyle} from "../../../styles";
+import CompanyListItem from "../../../components/signedIn/orders/order/companyListItem";
 
 interface orderData {
     order: order
 }
-
 interface orderProductData {
     orderProduct: orderProduct
 }
 
 export default function Order({route:{params:{id}}, navigation:{navigate, setOptions}}) {
-    setOptions({
-        title: "Order #"+id
-    });
+    setOptions({title: "Order #"+id});
 
-    const {data, loading, error} = useQuery<orderData>(order_query, {variables:{id: id}});
+    const {data, loading, error, refetch} = useQuery<orderData>(
+        order_query, {
+            variables:{id: id},
+            pollInterval: 10000
+        }
+        );
 
     if (loading) return null;
-
     if (error) return <Text>{error}</Text>;
 
     const {order} = data;
 
-    console.log(order);
-
     const renderOrderProduct = ({item:{orderProduct}}:{item:orderProductData}) =>(
         <ListItem
             title={orderProduct.product.code}
-            subtitle={orderProduct.quantity + " x " + orderProduct.product.nowPrice.price}
-            rightTitle={orderProduct.total.toString() + " zł"}
+            subtitle={orderProduct.quantity + " x " + orderProduct.product.nowPrice.price.toFixed(2) + ' zł'}
+            rightTitle={orderProduct.total.toFixed(2) + " zł"}
             chevron
             bottomDivider
         />
     );
 
     return(
-        <Card>
-            <ListItem
-                title={"Company"}
-                subtitle={"Company"}
-            />
-            <Card>
-                <Button title={"Add Product"}/>
-                <FlatList
-                    data={order.orderProducts.map(orderProduct=>({orderProduct, key:orderProduct.id.toString()}))}
-                    renderItem={renderOrderProduct}
-                />
-            </Card>
-            <ListItem
-                title={"Subtotal"}
-                rightTitle={order.subtotal.toString()+ " zł"}
-                bottomDivider
-            />
-            <ListItem
-                title={"Delivery"}
-                rightTitle={order.deliveryCost.toString() + " zł"}
-                bottomDivider
-            />
-            <ListItem
-                title={"Total"}
-                rightTitle={order.total.toString()+ " zł"}
-            />
-        </Card>
+        <View style={tabScreenStyle.container}>
+            <ScrollView refreshControl={<RefreshControl refreshing={loading} onRefresh={refetch}/> }>
+                <Card>
+                    <CompanyListItem orderId={order.id} company={order.company} onRefresh={refetch}/>
+                    <Card>
+                        <Button title={"Add Product"} onPress={()=>navigate("SearchStack", {
+                            screen: 'searchProduct',
+                            params: {
+                                orderId: id,
+                                orderProducts: order.orderProducts
+                            }
+                        })}/>
+                        <FlatList
+                            data={order.orderProducts.map(orderProduct=>({orderProduct, key:orderProduct.id.toString()}))}
+                            renderItem={renderOrderProduct} scrollEnabled={false}
+                        />
+                    </Card>
+                    <ListItem
+                        title={"Subtotal"}
+                        rightTitle={order.subtotal.toFixed(2)+ " zł"}
+                        bottomDivider
+                    />
+                    <ListItem
+                        title={"Delivery"}
+                        rightTitle={order.deliveryCost.toFixed(2) + " zł"}
+                        bottomDivider
+                    />
+                    <ListItem
+                        title={"Total"}
+                        rightTitle={order.total.toFixed(2)+ " zł"}
+                    />
+                </Card>
+            </ScrollView>
+
+        </View>
     );
 }
