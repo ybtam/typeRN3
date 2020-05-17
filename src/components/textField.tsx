d042b6818be21297d54d51c053af0225b72d3574
yitam <tam.yi3d@gmail.com>
2020-05-17 18:54:12 +0200

    turned it into a funciton

--- src/components/textField.tsx
+++ src/components/textField.tsx
@@ -12,71 +12,85 @@ const styles = StyleSheet.create({
 
 });
 
-//todo make it work later
-// export default function TextField({value, secureTextEntry = false, name, placeholder, onChangeText}) {
-//     function onChange({value}) {
-//         console.log(name);
-//         if (name == 'email'){
-//             onChangeText(name, value.trim());
-//         }else onChangeText(name, value);
-//     }
-//     if (name == 'email') {
-//         return (
-//             <Input
-//                 onChangeText={onChange}
-//                 value={value}
-//                 style={styles.field}
-//                 placeholder={placeholder}
-//                 autoCapitalize="none"
-//             />
-//         );
-//     }
-//     return (
-//         <Input
-//             onChangeText={onChangeText}
-//             value={value}
-//             style={styles.field}
-//             placeholder={placeholder}
-//             secureTextEntry={!!secureTextEntry}
-//         />
-//     );
-// }
-
-export default class TextField extends React.PureComponent {
-    onChangeText = (value) => {
-        // @ts-ignore
-        const {onChangeText, name} = this.props;
-
-        if (name == 'email'){
-            onChangeText(name, value.trim());
-        }else onChangeText(name, value);
-    };
-
-    render(): any {
-        // @ts-ignore
-        const { value, secureTextEntry, name, placeholder } = this.props;
-
-        if (name == 'email') {
-            return (
-                <Input
-                    onChangeText={this.onChangeText}
-                    value={value}
-                    style={styles.field}
-                    placeholder={placeholder}
-                    autoCapitalize="none"
-                />
-            );
-        }
+export default function TextField({value, secureTextEntry = false, name, placeholder, onChangeText}) {
 
 
+    function onChange (value) {
+        if (name == 'email') onChangeText(name, value.trim());
+        else onChangeText(name, value);
+    }
+
+    if (name == 'phone') {
         return (
             <Input
-                onChangeText={this.onChangeText}
+                onChangeText={onChange}
                 value={value}
                 style={styles.field}
                 placeholder={placeholder}
-                secureTextEntry={!!secureTextEntry}
+                autoCapitalize="none"
+                keyboardType={"numeric"}
             />
         );
     }
+
+    if (name == 'email') {
+        return (
+            <Input
+                onChangeText={onChange}
+                value={value}
+                style={styles.field}
+                placeholder={placeholder}
+                autoCapitalize="none"
+            />
+        );
+    }
+
+    return (
+        <Input
+            onChangeText={onChange}
+            value={value}
+            style={styles.field}
+            placeholder={placeholder}
+            secureTextEntry={!!secureTextEntry}
+        />
+    );
 }
+
+// export default class TextField extends React.PureComponent {
+//     onChangeText = (value) => {
+//         // @ts-ignore
+//         const {onChangeText, name} = this.props;
+//
+//         if (name == 'email'){
+//             onChangeText(name, value.trim());
+//         }else onChangeText(name, value);
+//     };
+//
+//     render(): any {
+//         // @ts-ignore
+//         const { value, secureTextEntry, name, placeholder } = this.props;
+//
+//         if (name == 'email') {
+//             return (
+//                 <Input
+//                     onChangeText={this.onChangeText}
+//                     value={value}
+//                     style={styles.field}
+//                     placeholder={placeholder}
+//                     autoCapitalize="none"
+//                 />
+//             );
+//         }
+//
+//
+//         return (
+//             <Input
+//                 onChangeText={this.onChangeText}
+//                 value={value}
+//                 style={styles.field}
+//                 placeholder={placeholder}
+//                 secureTextEntry={!!secureTextEntry}
+//             />
+//         );
+//     }
+// }
