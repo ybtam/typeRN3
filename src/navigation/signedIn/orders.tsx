306e7d8dc26fb6731f6132c524989193d9a30f1b
yitam <tam.yi3d@gmail.com>
2020-05-19 23:21:42 +0200

    added new screens

--- src/navigation/signedIn/orders.tsx
+++ src/navigation/signedIn/orders.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Orders from "../../screens/signedIn/orders/orders";
 import CreateOrder from "../../screens/signedIn/orders/createOrder";
 import Order from "../../screens/signedIn/orders/order";
+import QRCodeReader from "../../screens/signedIn/orders/QRCodeReader";
+import ParcelInfo from "../../screens/signedIn/orders/parcelnfo";
 
 const orders = createStackNavigator();
 
@@ -12,6 +14,8 @@ export default function ordersStack() {
             <orders.Screen name={"Orders"} component={Orders}/>
             <orders.Screen name={"Order"} component={Order}/>
             <orders.Screen name={"CreateOrder"} component={CreateOrder} options={{title:"Create Order"}}/>
+            <orders.Screen name={"ShippingScan"} component={QRCodeReader}/>
+            <orders.Screen name={"ParcelInfo"} component={ParcelInfo}/>
         </orders.Navigator>
     );
 }
